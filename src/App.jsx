e26efdbcc6521729454a11f0c6a505cd62c67441
shidqi29 <shidqiamirulhaq@gmail.com>
2023-09-01 21:41:58 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-[100vh] bg-slate-500 p-2 text-white">
+      <h1 className="text-3xl">Cek Ketersediaan Kamar Tidur Rumah Sakit</h1>
+    </div>
+  );
+}
+
+export default App;
