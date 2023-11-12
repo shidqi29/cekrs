1758faeee0f8c11aae0439613df4505935399244
shidqi29 <shidqiamirulhaq@gmail.com>
2023-11-12 20:01:33 +0700

    feat: add info phone & maps to detail hospital

--- src/pages/DetailHospital/index.jsx
+++ src/pages/DetailHospital/index.jsx
@@ -1,4 +1,4 @@
-import { MapPin } from "@phosphor-icons/react";
+import { MapPin, MapTrifold, Phone } from "@phosphor-icons/react";
 import { useParams } from "react-router-dom";
 
 import { BedDetailCard } from "@components/fragments";
@@ -8,21 +8,45 @@ import { useFetchData } from "@hooks/useFetchData";
 export const DetailHospital = () => {
   const { id } = useParams();
 
-  const { data } = useFetchData(`/get-bed-detail?hospitalid=${id}&type=2`);
+  const { data: detailHospital } = useFetchData(
+    `/get-bed-detail?hospitalid=${id}&type=2`,
+  );
+
+  const { data: hospitalMap } = useFetchData(
+    `/get-hospital-map?hospitalid=${id}`,
+  );
 
   return (
     <>
       <Content title="Detail Rumah Sakit">
         <div className="my-2 flex flex-col gap-y-1">
-          <h3 className="text-2xl font-bold">{data?.data?.name}</h3>
+          <h3 className="text-2xl font-bold">{detailHospital?.data?.name}</h3>
           <div className="flex items-center gap-x-2">
             <div>
               <MapPin size={26} weight="fill" />
             </div>
-            <span>{data?.data?.address}</span>
+            <span>{detailHospital?.data?.address}</span>
+          </div>
+          <div className="my-2 flex flex-col gap-2 md:flex-row">
+            <a
+              className="btn flex items-center gap-x-2 capitalize"
+              href={`tel:${detailHospital?.data?.phone}`}
+            >
+              <Phone size={26} weight="fill" />
+              <span className="">{detailHospital?.data?.phone}</span>
+            </a>
+            <a
+              className="btn flex items-center gap-x-2 capitalize"
+              href={hospitalMap?.data?.gmaps}
+              rel="noreferrer"
+              target="_blank"
+            >
+              <MapTrifold size={26} weight="fill" />
+              <span className="">Google Maps</span>
+            </a>
           </div>
           <div className="flex flex-col justify-center gap-y-8">
-            {data?.data?.bedDetail?.map((item, idx) => (
+            {detailHospital?.data?.bedDetail?.map((item, idx) => (
               <BedDetailCard key={idx} data={item} />
             ))}
           </div>
